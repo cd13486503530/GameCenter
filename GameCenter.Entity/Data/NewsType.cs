db2d21b51102920e60b86cf7df668dd97b2b3934
陈栋 <chend@xuele.net>
2016-07-12 13:08:08 +0800

    add newstype and partner

---
+++ GameCenter.Entity/Data/NewsType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCenter.Entity.Data
+{
+    [Table("t_NewsType", Schema = "dbo")]
+    public class NewsType
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(32)]
+        [Required, Column(TypeName = "varchar")]
+        public string Name { get; set; }
+    }
+}
