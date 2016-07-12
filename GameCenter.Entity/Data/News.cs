b4b1785689ee5b484f3b90c191f179bdadddf1c1
陈栋 <chend@xuele.net>
2016-07-12 13:22:58 +0800

    add news

---
+++ GameCenter.Entity/Data/News.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCenter.Entity.Data
+{
+    [Table("t_News", Schema = "dbo")]
+    public class News
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(64)]
+        [Required, Column(TypeName = "varchar")]
+        public string Title { get; set; }
+
+        [Column(TypeName = "varchar(max)")]
+        public string Contents { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public int NewsType { get; set; }
+
+        [MaxLength(64)]
+        [Column(TypeName = "varchar")]
+        public string Tag { get; set; }
+
+        [MaxLength(64)]
+        [Column(TypeName = "varchar")]
+        public string Author { get; set; }
+    }
+}
