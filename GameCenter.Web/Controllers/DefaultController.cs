3d7340f3a1b2fee260563b826eb7ed389b8cfcdf
taohaiyang <983027156@qq.com>
2016-07-13 21:19:24 +0800

    info

--- GameCenter.Web/Controllers/DefaultController.cs
+++ GameCenter.Web/Controllers/DefaultController.cs
@@ -13,5 +13,10 @@ namespace GameCenter.Web.Controllers
         {
             return View();
         }
+
+        public ActionResult INFO()
+        {
+            return View();
+        }
     }
 }
